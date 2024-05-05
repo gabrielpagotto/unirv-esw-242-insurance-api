2f8cdc386a688cc9baf506f7e3f113c67d993a83
Gabriel Pagotto <gabriel.batista@tradexsolutions.com.br>
2024-05-05 14:58:19 -0300

    Add requirements.txt and change docs url

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from src.features.credit_score import handler as credit_score_handler
 
-app = FastAPI()
+app = FastAPI(docs_url="/swagger")
 
 app.include_router(credit_score_handler.router_v1)
